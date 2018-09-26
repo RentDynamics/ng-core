7fc200882dda088e88f6a5bedb11a8576ab1177e
Chase Gibbons <chase@rentdynamics.com>
2018-09-26 15:35:47 -0600

    fix(auth.guard.ts, core-logout.component.ts): made it so logout component redirects the user to the base path onInit, also changed the auth.guard.ts to not add a return url when the path includes "logout" or "logoff", to prevent an infinite loop

--- src/lib/auth/core-logout/core-logout.component.ts
+++ src/lib/auth/core-logout/core-logout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'rd-core-logout',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoreLogoutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+      this.router.navigateByUrl('/');
   }
 
 }
