f13b090c4b588f4bc17b65b09c8b5172f03a3abb
Chase Gibbons <chase@rentdynamics.com>
2018-09-27 11:45:00 -0600

    fix(core-logout.component.html): made it so it would not show logout worksgit add . whenever you logoff \n also exposed a default core-auth-routing.module within core-auth.module.ts

--- src/lib/auth/core-auth.module.ts
+++ src/lib/auth/core-auth.module.ts
@@ -8,7 +8,7 @@ import { CoreLoginComponent } from './core-login/core-login.component';
 import { CoreSubmitNewPasswordComponent } from './core-submit-new-password/core-submit-new-password.component';
 import { CoreLogoutComponent } from './core-logout/core-logout.component';
 import { CoreLogoutResolve } from './core-logout/core-logout.resolve';
-// import { CoreAuthRoutingModule } from './core-auth-routing.module';
+import { CoreAuthRoutingModule } from './core-auth-routing.module';
 
 
 @NgModule({
@@ -33,7 +33,7 @@ import { CoreLogoutResolve } from './core-logout/core-logout.resolve';
     CoreSubmitNewPasswordComponent,
     CoreLogoutComponent,
     // FormsModule,
-    // CoreAuthRoutingModule
+    CoreAuthRoutingModule
   ]
 })
 export class CoreAuthModule {}
