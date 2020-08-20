a379a3b1b602a9dbdc855631f5b796794b1ab677
Sam Christensen <sam@rentdynamics.com>
2020-08-20 09:16:41 -0600

    fix(base api service): Update api service to calculate nonce after observable is hot, instead of imm

--- src/lib/shared/shared/api.service.ts
+++ src/lib/shared/shared/api.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Http, RequestOptionsArgs, URLSearchParams, Response } from '@angular/http';
-import { BehaviorSubject, Observable, ReplaySubject } from 'rxjs';
+import { BehaviorSubject, Observable, ReplaySubject, of } from 'rxjs';
 
 import { extend } from '../../helpers';
 import { AuthService } from './auth.service';
+import { map, switchMap } from 'rxjs/operators';
 
 
 @Injectable()
@@ -14,51 +15,61 @@ export class ApiService {
   get(endpoint: string, options: RequestOptionsArgs = {}): Observable<any> {
     endpoint = this.getSvcRoute() + endpoint;
     let url = this.getHost() + endpoint;
-    let headers = this.authService.getAuthHeaders(endpoint);
 
-    return this.http.get(url, extend({
-      headers: headers
-    }, options));
+    return of({}).pipe(
+      map(() => this.authService.getAuthHeaders(endpoint)),
+      switchMap(headers => this.http.get(url, extend({
+        headers: headers
+      }, options)))
+    );
   }
 
   put(endpoint: string, body: any, options: RequestOptionsArgs = {}): Observable<any> {
     endpoint = this.getSvcRoute() + endpoint;
     let url = this.getHost() + endpoint;
-    let headers = this.authService.getAuthHeaders(endpoint, body);
 
-    return this.http.put(url, body, extend({
-      headers: headers
-    }, options));
+    return of({}).pipe(
+      map(() => this.authService.getAuthHeaders(endpoint, body)),
+      switchMap(headers => this.http.put(url, body, extend({
+        headers: headers
+      }, options)))
+    );
   }
 
   post(endpoint: string, body: any, options: RequestOptionsArgs = {}): Observable<any> {
     endpoint = this.getSvcRoute() + endpoint;
     let url = this.getHost() + endpoint;
-    let headers = this.authService.getAuthHeaders(endpoint, body);
 
-    return this.http.post(url, body, extend({
-      headers: headers
-    }, options));
+    return of({}).pipe(
+      map(() => this.authService.getAuthHeaders(endpoint, body)),
+      switchMap(headers => this.http.post(url, body, extend({
+        headers: headers
+      }, options)))
+    );
   }
 
   postWithoutAuth(endpoint: string, body: any, options: RequestOptionsArgs = {}): Observable<any> {
     endpoint = this.getSvcRoute() + endpoint;
     let url = this.getHost() + endpoint;
-    let headers = this.authService.getAuthHeadersWithoutAuth(endpoint, body);
 
-    return this.http.post(url, body, extend({
-      headers: headers
-    }, options));
+    return of({}).pipe(
+      map(() => this.authService.getAuthHeadersWithoutAuth(endpoint, body)),
+      switchMap(headers => this.http.post(url, body, extend({
+        headers: headers
+      }, options)))
+    );
   }
 
   delete(endpoint: string, options: RequestOptionsArgs = {}): Observable<any> {
     endpoint = this.getSvcRoute() + endpoint;
     let url = this.getHost() + endpoint;
-    let headers = this.authService.getAuthHeaders(endpoint);
 
-    return this.http.delete(url, extend({
-      headers: headers
-    }, options));
+    return of({}).pipe(
+      map(() => this.authService.getAuthHeaders(endpoint)),
+      switchMap(headers => this.http.delete(url, extend({
+        headers: headers
+      }, options)))
+    );
   }
 
   errorHandler(error) {
